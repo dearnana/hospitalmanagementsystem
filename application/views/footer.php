bbec562fe58b129cd22f023d7fd63abc0dc10ec0
dearnana <stickyfinguz.angelo@gmail.com>
2023-08-15 09:09:09 +0000

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+    <div style="clear:both;color:rgb(0,0,0); padding:20px; bold">
+
+    	<hr /><center>&copy; <?php echo date ('Y'); ?> <strong>Online Hospital Management System</strong></center>
+
+    </div>
